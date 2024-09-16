4d9b6f0b67bcba718e6f315672df3e55b30ba6f5
FEX <felipefeferibeiro12@gmail.com>
2024-09-16 16:59:40 -0300

    knhjwdsudfdw

---
+++ cadastro.js
@@ -0,0 +1,22 @@
+// Sistema de cadastro
+prompt = require('prompt-sync')()
+
+console.log("Seja bem-vindo! Vamos começar seu cadastro agora mesmo.")
+
+const nome = prompt('Qual seu nome? ')
+
+
+const data = prompt('Qual sua data de nascimento? ')
+
+
+const email = prompt('Digite seu email: ')
+
+
+const senha = prompt('Digite uma senha: ')
+
+
+const telefone = prompt('Digite seu número de telefone: ')
+
+
+const endereco = prompt('Digite seu endereço: ')
+
