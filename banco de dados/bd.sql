b46a60a41d31533e9a25d1c402814ed1a7305135
érica <141285221+ericajahnke@users.noreply.github.com>
2024-10-25 19:58:02 -0300

    Projeto Integrador.

--- banco de dados/bd.sql
+++ banco de dados/bd.sql
@@ -25,7 +25,10 @@ insert into registro (matricula, nomecompleto, nascimento, telefone, email, senh
 ('4', 'João Vitor Pereira', '2005-04-30', '51982736721', 'joaovpereira@gmail.com', 'joaoblunt'),
 ('5', 'Rafael Barboza Balleste', '2002-11-10', '55991027434', 'barbozaball@gmail.com', 'malphiteap'),
 ('6', 'Lucas Almeida', '2006-08-17', '53981173546', 'luscasalmeida@gmail.com', 'freefire'),
-('7', 'Bianca Marino', '1999-08-01', '55923450124', 'bibimar@gmail.com', 'boatardeneyma');
+('7', 'Bianca Marino', '1999-08-01', '55923450124', 'bibimar@gmail.com', 'boatardeneyma'),
+('8', 'Mariana Barboza Balleste', '2006-08-12', '55938467624', 'mariball@gmail.com', 'sabcarpenter'),
+('9', 'Geovana da Silva', '2002-03-24', '55647829456', 'geodiva@gmail.com', 'paodebatata4592'),
+('10', 'Gabriele Santos', '2005-09-30', '53982635184', 'bibimar@gmail.com', 'coxinha123');
 
 select * from registro;
 
@@ -36,7 +39,10 @@ insert into acesso (matricula, email, senha) values
 ('4', 'joaovpereira@gmail.com', 'joaoblunt'),
 ('5', 'barbozaball@gmail.com', 'malphiteap'),
 ('6', 'luscasalmeida@gmail.com', 'freefire'),
-('7', 'bibimar@gmail.com', 'boatardeneyma');
+('7', 'bibimar@gmail.com', 'boatardeneyma'),
+('8', 'mariball@gmail.com', 'sabcarpenter'),
+('9', 'geodiva@gmail.com', 'paodebatata4592'),
+('10', 'bibimar@gmail.com', 'coxinha123');
 
 select * from acesso;
 
@@ -77,7 +83,10 @@ insert into aluno (matricula, nomecompleto, frequencia, totalresgates, totalpont
 ('4', 'João Vitor Pereira', '-75%', '4', '463'),
 ('5', 'Rafael Barboza Balleste', '-75%', '5', '507'),
 ('6', 'Lucas Almeida', '+75%', '3', '414'),
-('7', 'Bianca Marino', '-75%', '2', '552');
+('7', 'Bianca Marino', '-75%', '2', '552'),
+('8', 'Mariana Barboza Balleste', '-75%', '2', '504'),
+('9', 'Geovana da Silva', '-75%', '1', '301'),
+('10', 'Gabriele Santos', '+75%', '4', '383');
 
 select * from aluno;
 
@@ -100,7 +109,10 @@ insert into resgate (idresgate, idproduto, matricula, nomealuno, nomeproduto, po
 ('4', '1', '5', 'Rafael Barboza Balleste', 'Whey 900g', '463', '2024-10-20'),
 ('5', '2', '3', 'Lucas Pouey', 'Whey 1kg', '507', '2024-09-13'),
 ('6', '7', '7', 'Bianca Marino', 'Munhequeira', '414', '2024-10-28'),
-('7', '8', '6', 'Lucas Almeida', 'Strap', '552', '2024-10-24');
+('7', '8', '6', 'Lucas Almeida', 'Strap', '552', '2024-10-24'),
+('8', '3', '10', 'Gabriele Santos', 'Creatina 150g', '383', '2024-10-27'),
+('9', '5', '9', 'Geovana da Silva', 'Creatina 500g', '301', '2024-10-16'),
+('10', '4', '8', 'Mariana Barboza Balleste', 'Creatina 300g', '504', '2024-10-22');
 
 select * from resgate;
 
@@ -146,6 +158,9 @@ insert into userinfo (matricula, idade, altura, peso, sexo, atividadefisica) val
 ('4', '19', '160', '58', 'Masculino', 'Nula'),
 ('5', '21', '180', '70', 'Masculino', 'Regular'),
 ('6', '18', '170', '70', 'Masculino', 'Ativa'),
-('7', '25', '178', '58', 'Feminino', 'Regular');
+('7', '25', '178', '58', 'Feminino', 'Regular'),
+('8', '18', '155', '48', 'Feminino', 'Regular'),
+('9', '22', '169', '59', 'Feminino', 'Nula'),
+('10', '19', '173', '54', 'Feminino', 'Ativa');
 
 select * from userinfo;
